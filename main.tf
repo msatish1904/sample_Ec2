b2020691bd6ad26687f01879bedc562f0a585d21
msatish1904 <99471611+msatish1904@users.noreply.github.com>
2024-02-19 23:56:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,5 @@
 provider "aws" {
   region = var.region
-  aws_access_key = AKIAW3MEFAIS5M4RLM7Y
-  aws_secret_key = qRm5ZoLDWBQtva0YyxrSZZ0hD2JHdxWcSMLGGxRy
-  
 }
 
 resource "aws_instance" "example" {
