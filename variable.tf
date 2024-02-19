8764de773ac1ce62684417351895b3118a5702ab
msatish1904 <99471611+msatish1904@users.noreply.github.com>
2024-02-19 23:58:06 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -7,6 +7,7 @@ variable "region" {
 variable "ami_id" {
   description = "The AMI ID to use for the EC2 instance."
   type        = string
+  default     = "ami-0e731c8a588258d0d"
 }
 
 variable "instance_type" {
