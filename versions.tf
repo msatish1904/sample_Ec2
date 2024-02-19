80e6ac3db85533022cb8c6db43d40d5aa3984278
msatish1904 <99471611+msatish1904@users.noreply.github.com>
2024-02-19 23:58:52 +0530

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.1"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+  default_tags {
+    tags = {
+      "terraform-managed" = "true"
+    }
+  }
+}
